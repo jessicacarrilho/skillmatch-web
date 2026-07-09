93180a1c6cdf84c70a8fe7cc2d92fa66d97a368b
Jessica Carrilho <jessicavasconcelos.suporte@gmail.com>
2026-07-09 14:36:24 -0300

    feito banco de dados json

---
+++ assets/scripts/motor.js
@@ -0,0 +1,13 @@
+/*Motor de compatibilidade de vagas*/
+
+export class Vaga {
+    constructor(id, titulo, empresa, area, habilidadesRequisitadas, experienciaMeses) {
+        this.id = id;
+        this.titulo = titulo;
+        this.empresa = empresa;
+        this.area = area;
+        this.habilidadesRequisitadas = habilidadesRequisitadas;
+        this.experienciaMeses = experienciaMeses;
+    }
+}
+
